b13408a3d0cc883ac17cd8bc0352ad0fa6efe3e4
davidhapeman <david@hapeman.com>
2016-06-06 15:14:10 +0000

    David Hapeman Solution - Monday
    Enclosed is the solution to the register.  Please let me know if you have any issues.   The program can be installed from the following web site.
    
    http://www.hapemansoftwaresolutions.com/softwareinstalls/Demo/Demo.html

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Models.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Models
+    {
+        public class denominations
+        {
+            public Boolean b100 { get; set; }
+            public Boolean b50  { get; set; }
+            public Boolean b20  { get; set; }
+            public Boolean b10  { get; set; }
+            public Boolean b5   { get; set; }
+            public Boolean b1   { get; set; }
+            public Boolean bC25 { get; set; }
+            public Boolean bC10 { get; set; }
+            public Boolean bC5  { get; set; }
+            public Boolean bC1  { get; set; }
+        }
+
+        public class denomOuts : List<denomOut>
+        {
+            public denomOuts() { }
+            public denomOuts(List<denomOut> denomOuts) : base(denomOuts) { }
+        }
+        public class denomOut
+        {
+            public string outChange { get; set; }
+        }
+
+        public class fileInputs : List<fileInput>
+        {
+            public fileInputs() { }
+            public fileInputs(List<fileInput> fileInputs) : base(fileInputs) { }
+        }
+        public class fileInput
+        {
+            public decimal amountOwed { get; set; }
+            public decimal amountPaid { get; set; }
+        }
+
+    }
+}
